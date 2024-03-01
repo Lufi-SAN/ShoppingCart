b46ee13fb1128ed7acb4f7969c6b842b2cbc8e0a
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2024-03-01 12:03:39 +0100

    Change cart state to cartItemCount with type: array

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import './App.css'
 import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [cart, setCart] = useState(0)
+  const [cartItems, setCartItems] = useState([])
   
   return (
     <>
-      <Navbar cart={cart}/>
+      <Navbar cartItemCount={cartItems.length}/>
       <Outlet/>
     </>
   )
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,15 +4,15 @@ import searchImage from './assets/search.png'
 import shopBasketImage from './assets/shopping_basket.png'
 import './navbar.css'
 
-const Navbar = ({cart}) => {
+const Navbar = ({cartItemCount}) => {
     return <>
             <div className='container'>
                 <div className="searchContainer">
                     <div><input type="text" /></div>
                     <div><img src={searchImage} alt="" /></div>
                 </div>
-                <div><Link to="/"><img src={homeImage} alt="Icon for link to homepage" /></Link></div>
-                <div id='cartContainer'><div><Link to="cart"><img src={shopBasketImage} alt="Icon for link to checkout" /></Link></div>{cart && <div id='cartNumber'>{cart}</div>}</div>
+                <Link to="/"><div><img src={homeImage} alt="Icon for link to homepage" /></div></Link>
+                <div className='cartContainer'><Link to="cart"><div><img src={shopBasketImage} alt="Icon for link to checkout" /></div>{cartItemCount && <div id='cartNumber'>{cartItemCount}</div>}</Link></div>
             </div>
            </>
 }
