673a97c020c20f8952ebfa6e0f7f0594bd92c5bb
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2024-04-07 13:39:34 +0100

    Fix the useProductsArray custom hook

--- src/Hooks/useProductsArray.js
+++ src/Hooks/useProductsArray.js
@@ -1,12 +1,14 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 export default function useProductsArray() {
-    let products = null;
+    const [products, setProducts] = useState(null);
+
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json => {products = json})
-            .catch(err => {console.log(`There was a ${err} error`); return []})
-    }, [])
-    return products;
+            .then(res => res.json())
+            .then(json => setProducts(json))
+            .catch(err => console.log(`There was a ${err} error`));
+    }, []);
+
+    return {products};
 }
