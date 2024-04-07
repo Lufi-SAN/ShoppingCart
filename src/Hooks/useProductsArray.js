16bcc471dd4f590db4a826f820f0866d4e8a3ab3
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2024-04-07 14:03:11 +0100

    Create error and loading states

--- src/Hooks/useProductsArray.js
+++ src/Hooks/useProductsArray.js
@@ -2,13 +2,22 @@ import { useState, useEffect } from 'react';
 
 export default function useProductsArray() {
     const [products, setProducts] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
-            .then(res => res.json())
+            .then(res => {
+                            if(res.status >= 400) {throw new Error("Server error")}
+                            return res.json()
+                        }
+                 )
             .then(json => setProducts(json))
-            .catch(err => console.log(`There was a ${err} error`));
+            .catch(err => {
+                            setError(err)
+                        })
+            .finally(setLoading(false));
     }, []);
 
-    return {products};
+    return {products, error, loading};
 }
