9355712f9c619c02ef6a1a0dd9a8c7ae8777fe4e
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2024-04-07 14:00:09 +0100

    Use params for routing instead of Outlet; Use custom useProdArr hook for conditional rendering

--- src/Routes/App.jsx
+++ src/Routes/App.jsx
@@ -1,20 +1,22 @@
 import { useState, createContext } from 'react'
 import Navbar from './Navbar.jsx'
 import '../Stylesheets/App.css'
-import { Outlet } from 'react-router-dom'
 import useProductsArray from '../Hooks/useProductsArray'
+import { useParams } from 'react-router-dom'
+import HomePage from './HomePage.jsx'
+import Cart from './Cart.jsx'
 
 export const Context = createContext({})
 
 function App() {
-  const products = useProductsArray();
-  const [productsArray, setProductsArray] = useState([])
   const [cartItems, setCartItems] = useState([])
-  
-  if (!products) {
-    return <div>Content Loading...</div>
-  } else {
-    setProductsArray(products);
+  const { products, error, loading } = useProductsArray();
+  const { name } = useParams()
+
+  if (loading) {
+    return <p>Content Loading...</p>
+  } else if(error) {
+    return <p>Had a network error</p>
   }
   
   
@@ -24,12 +26,12 @@ function App() {
         <nav><Navbar cartItemCount={cartItems.length}/></nav>
       </header>
       <main>
-        <Context.Provider value={{productsArray, cartItems, setCartItems}}>
-        <Outlet/>
+        <Context.Provider value={{products, cartItems, setCartItems}}>
+        {name === "cart" ? <Cart /> : <HomePage />}
         </Context.Provider>
       </main>
       <footer>
-        
+
       </footer>
     </>
   )
