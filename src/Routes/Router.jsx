c170dc48b3444971b15e5b8b126a1a0303c9652a
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2024-04-07 13:58:41 +0100

    Use dynamic segments for routing; remove children elements

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,25 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App'
-import Cart from './Cart'
-import HomePage from './HomePage'
 import ErrorElement from './ErrorElement'
 
 const Router = () => {
     const router = createBrowserRouter(
         [
             {
-                path: '/',
+                path: '/:name',
                 element: <App />,
-                children: [
-                    {
-                        path: 'cart',
-                        element: <Cart />
-                    },
-                    {
-                        path: 'home',
-                        element: <HomePage />,
-                    }
-                ],
                 errorElement: <ErrorElement />
             },
         ]
