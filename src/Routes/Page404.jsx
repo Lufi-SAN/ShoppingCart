5b3ddfa0c64f704a6e897918c32447ecaf1a763b
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2024-04-24 03:36:21 +0100

    Create loader, index and splash(I think thats what its called?) page

---
+++ src/Routes/Page404.jsx
@@ -0,0 +1,3 @@
+export default function Page404() {
+    return <h1>PAGE DOES NOT EXIST</h1>
+}
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,15 +1,33 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from './App'
+import Layout,{ loader as layoutLoader } from './Layout'
 import ErrorElement from './ErrorElement'
+import HomePage from "./HomePage";
+import Cart from "./Cart";
+import Page404 from "./Page404";
 
 const Router = () => {
     const router = createBrowserRouter(
         [
             {
-                path: '/:name',
-                element: <App />,
-                errorElement: <ErrorElement />
+                path: '/',
+                element: <Layout />,
+                errorElement: <ErrorElement />,
+                loader: layoutLoader,
+                children: [
+                    {
+                        index: true,
+                        element: <HomePage />
+                    },
+                    {
+                        path: 'cart',
+                        element: <Cart />
+                    }
+                ]
             },
+            {
+                path: '*',
+                element: <Page404 />
+            }
         ]
     )
     
